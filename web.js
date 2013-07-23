ded46f0cf87add984273b57f86f2bf8b19ac1d23
peeyush <peeyushagarwal1994@gmail.com>
2013-07-23 20:22:14 +0530

    Trying to print index.html

--- web.js
+++ web.js
@@ -1,3 +1,17 @@
+var http = require('http');
+var fs = require('fs');
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+});
+/*
 var express = require('express');
 var app = express();
 app.use(express.logger());
@@ -10,3 +24,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+*/
