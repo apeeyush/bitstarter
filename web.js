989292d2756874bb0f896ff22176afa6d4b55f2a
peeyush <peeyushagarwal1994@gmail.com>
2013-07-23 20:46:10 +0530

    Installed dependencies

--- web.js
+++ web.js
@@ -1,27 +1,20 @@
-var http = require('http');
+
+var express = require('express');
 var fs = require('fs');
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
-});
-/*
-var express = require('express');
 var app = express();
+
+var mybuf;
 app.use(express.logger());
 
+ mybuf=fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(mybuf.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-*/
+
